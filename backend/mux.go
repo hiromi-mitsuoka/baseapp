0a4d344916303b7c04b2af8e2010f78636d359b2
hiromi-mitsuoka <hiro.m.19981217@ezweb.ne.jp>
2022-08-24 08:50:05 +0900

    :books: Add comment of mux and handler

--- backend/mux.go
+++ backend/mux.go
@@ -2,6 +2,15 @@ package main
 
 import "net/http"
 
+// https://qiita.com/huji0327/items/c85affaf5b9dbf84c11e
+// muxとは
+// マルチプレクサ: ふたつ以上の入力をひとつの信号として出力する機構．通信分野では多重通信の入口の装置．muxと略される．
+// ハンドラ（Go言語における）: ServeHTTPというメソッドを持ったインターフェースのこと．
+
+// https://www.twihike.dev/docs/golang-web/handlers
+// マルチプレクサ：リクエストを受け付け、URLに対応するハンドラへ転送する
+// ハンドラ：クリエストに応じた処理をして、レスポンスを返却する
+
 // NOTE: 戻り値を*http.ServeMux型の値ではなく，http.Handlerインターフェースにしておくことで，内部実装に依存しない関数シグネチャになる
 func NewMux() http.Handler {
 	mux := http.NewServeMux()
