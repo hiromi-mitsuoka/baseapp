5491d3d82e411cb26de9eb233fa067e1081f6005
hiromi-mitsuoka <hiro.m.19981217@ezweb.ne.jp>
2022-08-23 20:12:04 +0900

    :books: Add comment abount env

--- backend/config/config.go
+++ backend/config/config.go
@@ -9,6 +9,7 @@ type Config struct {
 
 func New() (*Config, error) {
 	cfg := &Config{}
+	// https://github.com/caarlos0/env#example
 	// os.Getenv()との違いは，タグによってデフォルト値を設定できること
 	if err := env.Parse(cfg); err != nil {
 		return nil, err
